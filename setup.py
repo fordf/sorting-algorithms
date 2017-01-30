df9bcd2abe459f2050c7e5c452a08ca3a0c04d89
Ford Fowler <fordjfowler@gmail.com>
2017-01-30 13:13:06 -0800

    files created

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Setup data-structures module."""
+
+
+from setuptools import setup
+
+setup(
+    name="sorting-algorithms",
+    description="Python implementations of classic sorting algorithms",
+    version=0.2,
+    author=["Ford Fowler", "Claire Gatenby"],
+    author_email=["fordjfowler@gmail.com", "clairejgatenby@gmail.com"],
+    licencse="MIT",
+    package_dir={'': 'src'},
+    py_modules=["insertion_sort"],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "tox"]
+    }
+)

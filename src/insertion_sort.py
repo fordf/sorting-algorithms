82ea4fcc7fd31a344871e81cbbc4c0fc50723bc7
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-30 14:19:47 -0800

    added timeit to get how long the sorting function takes for different inputs

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -9,3 +9,25 @@ def insertion_sort(iterable):
             iterable[new], iterable[new - 1] = iterable[new - 1], iterable[new]
             new = new - 1
     return iterable
+
+
+if __name__ == '__main__':
+    import timeit
+    from random import randint
+
+    input1 = [2, 1]
+    input2 = [randint(0, 100000) for i in range(1000)]
+
+    print('Insertion sort is a sorting algorith that builds a sorted list from and input string one line at a time.')
+
+    print('Timing for input [2, 1]: \n' +
+
+          str(timeit.timeit(stmt="insertion_sort(input1)",
+                            setup='from __main__ import insertion_sort, input1',
+                            number=500)) +
+          '\naverage time over 500 runs')
+    print('Timing for [randint(0, 100000) for i in range(1000)]:\n' +
+          str(timeit.timeit(stmt="insertion_sort(input2)",
+                            setup='from __main__ import insertion_sort, input2',
+                            number=500)) +
+          '\naverage time over 500 runs')

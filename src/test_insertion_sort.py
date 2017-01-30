e82f6911cda51d489e6690f95f57b80249125c6b
Ford Fowler <fordjfowler@gmail.com>
2017-01-30 14:30:32 -0800

    test floats

--- src/test_insertion_sort.py
+++ src/test_insertion_sort.py
@@ -6,12 +6,13 @@ LISTS = [
     ([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
     ([4, 2, 4, 1, 3, 5, 6, 3], [1, 2, 3, 3, 4, 4, 5, 6]),
     ([-1, -10, -3, 2, 139, -101, 192], [-101, -10, -3, -1, 2, 139, 192]),
+    ([3.3, 1.12, 42, 19.9999999999, 19.999], [1.12, 3.3, 19.999, 19.9999999999, 42]),
     (['aa', 'b', 'a', 'ab'], ['a', 'aa', 'ab', 'b']),
 ]
 
 
 @pytest.mark.parametrize('unsorted_lst, sorted_lst', LISTS)
 def test_insertion_sort(unsorted_lst, sorted_lst):
-    """."""
+    """Insertion sort should sort lists."""
     from insertion_sort import insertion_sort
     assert insertion_sort(unsorted_lst) == sorted_lst

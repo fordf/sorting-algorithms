961a19721d67bc180b078f6d68d72d66b11a99da
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-01 13:38:43 -0800

    added timeit for quicksort

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -19,3 +19,25 @@ def partition(lst):
             swap_at += 1
     lst[-1], lst[swap_at] = lst[swap_at], lst[-1]
     return swap_at
+
+
+if __name__ == '__main__':  
+    import timeit
+    from random import randint
+
+    input1 = [2, 1]
+    input2 = [randint(0, 100000) for i in range(1000)]
+
+    print('Quick sort is an efficient, general-purpose, comparison-based sorting algorithm.')
+
+    print('Timing for input [2, 1]: \n' +
+
+          str(timeit.timeit(stmt="quick_sort(input1)",
+                            setup='from __main__ import quick_sort, input1',
+                            number=500)) +
+          '\naverage time over 500 runs')
+    print('Timing for [randint(0, 100000) for i in range(1000)]:\n' +
+          str(timeit.timeit(stmt="quick_sort(input2)",
+                            setup='from __main__ import quick_sort, input2',
+                            number=500)) +
+          '\naverage time over 500 runs')
